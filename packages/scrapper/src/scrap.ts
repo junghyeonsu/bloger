e80e146d52e698dd81f4d8eb716125eab71d15ef
junghyeonsu <jung660317@naver.com>
2023-11-13 10:47:47 +0900

    feat: puppeteer 순차적으로 실행

--- packages/scrapper/src/scrap.ts
+++ packages/scrapper/src/scrap.ts
@@ -27,49 +27,119 @@ export const scrapNaverEconomyHeadlineNews = async () => {
   // 헤드라인 뉴스 텍스트 컨테이너 가져오기
   const headlineNewsTextContainerSelector = ".sh_text";
   const headlineNewsTextContainers = await page.$$(headlineNewsTextContainerSelector);
-  const headlineNewsContents = await Promise.all(
-    headlineNewsTextContainers.map(async (headlineNewsTextContainer) => {
-      const title = await headlineNewsTextContainer.$eval("a", (a) => a.textContent);
-      const link = await headlineNewsTextContainer.$eval("a", (a) => a.getAttribute("href"));
-      const corp = await headlineNewsTextContainer.$eval(".sh_text_info > div", (div) => div.textContent);
-      let aiSummaryContent;
 
-      if (!link) return { title, link: undefined, corp };
+  const headlineNewsContents: HeadlineNewsContent[] = [];
 
-      // 뉴스 페이지 오픈
-      const newsPage = await browser.newPage();
-      await newsPage.goto(link);
-      await newsPage.waitForNetworkIdle();
+  for (const headlineNewsTextContainer of headlineNewsTextContainers) {
+    let aiSummaryContent;
+    const title = await headlineNewsTextContainer.$eval("a", (a) => a.textContent);
+    const link = await headlineNewsTextContainer.$eval("a", (a) => a.getAttribute("href"));
+    const corp = await headlineNewsTextContainer.$eval(".sh_text_info > div", (div) => div.textContent);
 
-      const aiSummaryButtonSelector = ".media_end_head_autosummary_button";
-      if (await newsPage.$(aiSummaryButtonSelector)) {
-        await newsPage.click(aiSummaryButtonSelector).then(() => console.log("클릭"));
-        await newsPage.waitForResponse((response) => response.url().includes("summary"));
-        await newsPage.waitForSelector(".media_end_head_autosummary_layer_tit");
-        const contentBody = await newsPage.$("._SUMMARY_CONTENT_BODY");
-        const contentInnerHTML = await contentBody?.evaluate((node) => node.innerHTML);
-        aiSummaryContent = contentInnerHTML;
-      }
+    if (!link || !title || !corp) {
+      headlineNewsContents.push({
+        title: title as string,
+        link: undefined,
+        corp: corp as string,
+        content: "",
+        summary: undefined,
+        aiSummary: undefined,
+      });
+      continue;
+    }
 
-      // 뉴스 본문 가져오기
-      const article = await newsPage.$("#dic_area");
-      const content = await article?.evaluate((node) => node.textContent);
-      const cleanContent = content?.replace(/\n/g, " ").replace(/\t/g, " ").replace(/\s\s+/g, " ").trim();
+    // 뉴스 페이지 오픈
+    const newsPage = await browser.newPage();
+    await newsPage.goto(link);
+    await newsPage.waitForNetworkIdle();
 
-      // 뉴스 요약 가져오기
-      const summarySelector = await newsPage.$(".media_end_summary");
-      if (!summarySelector) {
-        await newsPage.close();
-        return { title, link, corp, content: cleanContent, summary: undefined, aiSummary: aiSummaryContent };
-      }
+    const aiSummaryButtonSelector = ".media_end_head_autosummary_button";
+    if (await newsPage.$(aiSummaryButtonSelector)) {
+      await newsPage.click(aiSummaryButtonSelector).then(() => console.log("클릭"));
+      await newsPage.waitForResponse((response) => response.url().includes("summary"));
+      await newsPage.waitForSelector(".media_end_head_autosummary_layer_tit");
+      const contentBody = await newsPage.$("._SUMMARY_CONTENT_BODY");
+      const contentInnerHTML = await contentBody?.evaluate((node) => node.innerHTML);
+      aiSummaryContent = contentInnerHTML;
+    }
 
-      // 뉴스 요약이 있는 경우
-      const summaryContent = await summarySelector.evaluate((node) => node.textContent);
+    // 뉴스 본문 가져오기
+    const article = await newsPage.$("#dic_area");
+    const content = await article?.evaluate((node) => node.textContent);
+    const cleanContent = content?.replace(/\n/g, " ").replace(/\t/g, " ").replace(/\s\s+/g, " ").trim();
+
+    // 뉴스 요약 가져오기
+    const summarySelector = await newsPage.$(".media_end_summary");
+    if (!summarySelector) {
       await newsPage.close();
+      headlineNewsContents.push({
+        title,
+        link,
+        corp,
+        content: cleanContent as string,
+        summary: undefined,
+        aiSummary: aiSummaryContent,
+      });
+      continue;
+    }
 
-      return { title, link, corp, content: cleanContent, summary: summaryContent, aiSummary: aiSummaryContent };
-    }),
-  );
+    // 뉴스 요약이 있는 경우
+    const summaryContent = await summarySelector.evaluate((node) => node.textContent);
+    await newsPage.close();
+
+    headlineNewsContents.push({
+      title,
+      link,
+      corp,
+      content: cleanContent as string,
+      summary: summaryContent as string,
+      aiSummary: aiSummaryContent,
+    });
+  }
+
+  // const headlineNewsContents = await Promise.all(
+  //   headlineNewsTextContainers.map(async (headlineNewsTextContainer) => {
+  //     const title = await headlineNewsTextContainer.$eval("a", (a) => a.textContent);
+  //     const link = await headlineNewsTextContainer.$eval("a", (a) => a.getAttribute("href"));
+  //     const corp = await headlineNewsTextContainer.$eval(".sh_text_info > div", (div) => div.textContent);
+  //     let aiSummaryContent;
+
+  //     if (!link) return { title, link: undefined, corp };
+
+  //     // 뉴스 페이지 오픈
+  //     const newsPage = await browser.newPage();
+  //     await newsPage.goto(link);
+  //     await newsPage.waitForNetworkIdle();
+
+  //     const aiSummaryButtonSelector = ".media_end_head_autosummary_button";
+  //     if (await newsPage.$(aiSummaryButtonSelector)) {
+  //       await newsPage.click(aiSummaryButtonSelector).then(() => console.log("클릭"));
+  //       await newsPage.waitForResponse((response) => response.url().includes("summary"));
+  //       await newsPage.waitForSelector(".media_end_head_autosummary_layer_tit");
+  //       const contentBody = await newsPage.$("._SUMMARY_CONTENT_BODY");
+  //       const contentInnerHTML = await contentBody?.evaluate((node) => node.innerHTML);
+  //       aiSummaryContent = contentInnerHTML;
+  //     }
+
+  //     // 뉴스 본문 가져오기
+  //     const article = await newsPage.$("#dic_area");
+  //     const content = await article?.evaluate((node) => node.textContent);
+  //     const cleanContent = content?.replace(/\n/g, " ").replace(/\t/g, " ").replace(/\s\s+/g, " ").trim();
+
+  //     // 뉴스 요약 가져오기
+  //     const summarySelector = await newsPage.$(".media_end_summary");
+  //     if (!summarySelector) {
+  //       await newsPage.close();
+  //       return { title, link, corp, content: cleanContent, summary: undefined, aiSummary: aiSummaryContent };
+  //     }
+
+  //     // 뉴스 요약이 있는 경우
+  //     const summaryContent = await summarySelector.evaluate((node) => node.textContent);
+  //     await newsPage.close();
+
+  //     return { title, link, corp, content: cleanContent, summary: summaryContent, aiSummary: aiSummaryContent };
+  //   }),
+  // );
 
   await browser.close();
 
